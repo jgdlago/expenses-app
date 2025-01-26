d5798932bb69228ff16d4300cf658ec0b5242d87
João Dal Lago <joaogabrieldallago@hotmail.com>
2025-01-26 11:38:49 -0300

    :sparkles: feat(public): ajustes de alinhamento das barras de gastos

--- expenses/lib/components/chart_bar.dart
+++ expenses/lib/components/chart_bar.dart
@@ -16,8 +16,10 @@ class ChartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Text('R\$${value.toStringAsFixed(2)}'),
-        SizedBox(height: 5),
+        FittedBox(
+            child: Text('${value.toStringAsFixed(2)}'),
+        ),
+        const SizedBox(height: 5),
         Container(
           height: 60,
           width: 10,
