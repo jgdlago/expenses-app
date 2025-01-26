edb5b7426a1e39c85b3cadb46d087e4b3a987ceb
João Dal Lago <joaogabrieldallago@hotmail.com>
2025-01-26 18:08:24 -0300

    :sparkles: feat(public): adiciona exclusão da transação

--- expenses/lib/components/transaction_list.dart
+++ expenses/lib/components/transaction_list.dart
@@ -4,8 +4,9 @@ import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
+  final void Function(String) onRemove;
 
-  const TransactionList(this.transactions, {Key? key}) : super(key: key);
+  const TransactionList(this.transactions, this.onRemove, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -37,6 +38,11 @@ class TransactionList extends StatelessWidget {
               ),
               subtitle: Text(
                 DateFormat('d MMM y').format(tr.date),
+              ),
+              trailing: IconButton(
+                icon: const Icon(Icons.delete),
+                color: Theme.of(context).colorScheme.error,
+                onPressed: () => onRemove(tr.id),
               ),
             ),
           );
--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -89,6 +89,12 @@ class _MyHomePageState extends State<MyHomePage> {
     Navigator.of(context).pop();
   }
 
+  _removeTransaction(String id) {
+    setState(() {
+      _transactions.removeWhere((tr) => tr.id == id);
+    });
+  }
+
   _openTransactionFormModal(BuildContext context) {
     showModalBottomSheet(
       context: context,
@@ -115,7 +121,7 @@ class _MyHomePageState extends State<MyHomePage> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Chart(_recentTransactions),
-            TransactionList(_transactions),
+            TransactionList(_transactions, _removeTransaction),
           ],
         ),
       ),
